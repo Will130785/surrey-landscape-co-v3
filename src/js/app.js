ab4acf8ee743ed0f5ab9245ad4b9c22bffbb64d3
willconstable <will_constable@msn.com>
2020-06-24 08:41:02 +0100

    update

--- src/js/app.js
+++ src/js/app.js
@@ -7,17 +7,18 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "25px";
     document.querySelector(".logo-img").style.height = "10rem";
     document.querySelector(".logo-img").style.width = "20rem";
-    // document.getElementById("navbar").style.backgroundColor = "white";
+    document.getElementById("navbar").style.backgroundColor = "white";
   } else {
     // document.getElementById("navbar").style.padding = "80px 10px";
     document.getElementById("logo").style.fontSize = "35px";
     document.querySelector(".logo-img").style.height = "20rem";
     document.querySelector(".logo-img").style.width = "40rem";
-    // document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, .5)";
+    document.getElementById("navbar").style.backgroundColor = "rgba(255, 255, 255, .5)";
   }
 } 
 
 //Hamburger controls
+//The following code controls what happens when the user clicks on the hamburger button
 const hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener("click", e => {
@@ -26,17 +27,25 @@ hamburger.addEventListener("click", e => {
     if(hamburger.classList.contains("active")) {
         document.getElementById("navbar").style.height = "100vh";
         document.getElementById("navbar-right").style.display = "flex";
+        document.getElementById("navbar-right").style.visibility = "visible";
     } else {
         document.getElementById("navbar").style.height = "auto";
         document.getElementById("navbar-right").style.display = "none";
+        document.getElementById("navbar-right").style.visibility = "hidden";
     }
 })
 
+//If screen is resized whilst dropdown menu is active
 window.addEventListener("resize", e => {
     if(window.innerWidth > 900) {
-        document.getElementById("navbar-right").style.display = "flex";
+      hamburger.classList.remove("active");
+      document.getElementById("navbar-right").style.display = "flex";
+        document.getElementById("navbar-right").style.visibility = "visible";
         document.getElementById("navbar").style.height = "auto";
+        document.getElementById("navbar").style.backgroundColor = "rgba(255, 255, 255, .5)";
     } else {
-        document.getElementById("navbar-right").style.display = "none";
+      document.getElementById("navbar-right").style.display = "none";
+      document.getElementById("navbar-right").style.visibility = "hidden";
+      document.getElementById("navbar").style.backgroundColor = "white";
     }
 });
